2f21ec30bb80f3c24f1916f685090f5c256dab68
yantsong <songusepython@gmail.com>
2017-07-19 02:21:16 +0800

    add userpage css

--- app/containers/MainPage/UserPage/UserPage.jsx
+++ app/containers/MainPage/UserPage/UserPage.jsx
@@ -8,7 +8,28 @@ class UserPage extends Component {
     const {userInfo} = this.props;
         return (
             <div className = "user-page">
-                <h1> <a href="#"><img src={userInfo.avatar}/></a> <span>{userInfo.name}</span> <i>编辑</i>   </h1>
+                <h1> <a href="#"><img src={userInfo.avatar}/></a> <span>{userInfo.name}</span> <i>编辑</i>  </h1>
+                <ul>
+                    <li>
+                        <span>我发起的点映({userInfo.movie.length})</span> <i>></i>
+                    </li>
+                    <li>
+                        <span>我报名的点映(0)</span><i>></i>
+                    </li>
+                    <li>
+                        <span>我的关注</span><i>></i>
+                    </li>
+                    <li>
+                        <span>我收到的留言(0)</span><i>></i>
+                    </li>
+                    <li>
+                        <span>我想点映</span><i>></i>
+                    </li>
+                </ul>
+                <p>
+                    <i>?</i>
+                    常见问题解答
+                </p>
             </div>
         );
     }
